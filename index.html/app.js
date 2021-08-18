979270a5001137a04561aea9677f1a6e999c528c
Nahlaanajeeb <nahla.a@mfine.co>
2021-08-18 10:42:57 +0530

    mycommit

---
+++ index.html/app.js
@@ -0,0 +1,8 @@
+let num = document.getElementById("number");
+function addlist(){
+    if(num.value !=' '){
+        
+        output += `<li> ${num.value} </li>`;
+    }
+    document.getElementById("numberList").innerHTML = output;
+}
